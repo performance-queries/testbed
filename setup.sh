87c1c11c595ef4912fa555b37cf869383644f6fb
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-05 02:07:32 +0000

    Update instructions to include Spark installation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
-# Clone mininet
 sudo apt-get install git vim linux-generic-lts-vivid default-jdk default-jre
-# reboot here
+
+# reboot here for the new kernel to take effect
+
+# edit /etc/hosts to include the hostname of the EC2 instance.
+# Spark doesn't work otherwise; mininet whines
+
+# Mininet installation
 git clone git://github.com/mininet/mininet
 git checkout -b 2.2.1 2.2.1
 cd ..
 mininet/util/install.sh -a
 
+# Spark installation
+wget http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz
+tar xvzf spark-2.1.0-bin-hadoop2.7.tgz 
+cd spark-2.1.0-bin-hadoop2.7/
+./bin/run-example --master local[2] streaming.NetworkWordCount localhost 9999
