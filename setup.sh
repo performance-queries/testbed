7365da969dcfb6eb00991f6c84ac1b69fca0029a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-04 20:40:22 -0500

    Java dependency for Spark

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 # Clone mininet
-sudo apt-get install git vim linux-generic-lts-vivid
+sudo apt-get install git vim linux-generic-lts-vivid default-jdk default-jre
 # reboot here
 git clone git://github.com/mininet/mininet
 git checkout -b 2.2.1 2.2.1
 cd ..
 mininet/util/install.sh -a
+
