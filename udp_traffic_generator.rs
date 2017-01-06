8bdda5f6135c340b4026170fb4ff1df838430efb
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-06 12:56:16 -0500

    Sleep before sending data

--- udp_traffic_generator.rs
+++ udp_traffic_generator.rs
@@ -1,6 +1,12 @@
 use std::net::UdpSocket;
+use std::thread;
+use std::time;
 
 fn main() {
   let socket = UdpSocket::bind("127.0.0.1:3000").unwrap();
-  let _ = socket.send_to(&['a' as u8;3500], "127.0.0.1:4000");
+  let ten_millis = time::Duration::from_millis(10);
+  loop {
+    thread::sleep(ten_millis);
+    let _ = socket.send_to(&['a' as u8;350], "127.0.0.1:4000");
+  }
 }
