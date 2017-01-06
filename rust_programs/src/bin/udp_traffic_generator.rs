c9590a366ff401b699808ebbba5b945688a95dd1
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-06 13:44:15 -0500

    Generate random off time between 0 and 256 ms

--- rust_programs/src/bin/udp_traffic_generator.rs
+++ rust_programs/src/bin/udp_traffic_generator.rs
@@ -1,12 +1,12 @@
 use std::net::UdpSocket;
 use std::thread;
 use std::time;
+extern crate rand;
 
 fn main() {
   let socket = UdpSocket::bind("127.0.0.1:3000").unwrap();
-  let ten_millis = time::Duration::from_millis(10);
   loop {
-    thread::sleep(ten_millis);
+    thread::sleep(time::Duration::from_millis(rand::random::<u8>() as u64));
     let _ = socket.send_to(&['a' as u8;350], "127.0.0.1:4000");
   }
 }
