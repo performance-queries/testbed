d4510d53ec54fdfe1746fbf176c520352e5a79b2
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-06 10:35:23 -0500

    Explanatory comments

--- http_client.rs
+++ http_client.rs
@@ -5,10 +5,15 @@ use std::str;
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:80").unwrap();
 
-    // ignore the Result
+    // _ ignores the result
     let _ = stream.write(b"GET /loremipsum.txt\r\n");
 
+    // mutable array with 3000 elements, each initialized to zero
     let mut result = [0; 3000];
-    let _ = stream.read(&mut result[..]); // ignore here too
+
+    // &mut result creates a slice with all elements
+    // mut indicates that it can be mutated
+    let _ = stream.read(&mut result);
+
     print!("{}", str::from_utf8(&result).unwrap());
 }
