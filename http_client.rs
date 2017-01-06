ed7fc1e35afc5773fa221e1593e425918eb316ed
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-01-06 10:42:35 -0500

    Use as_bytes instead of b""

--- http_client.rs
+++ http_client.rs
@@ -6,7 +6,9 @@ fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:80").unwrap();
 
     // _ ignores the result
-    let _ = stream.write(b"GET /loremipsum.txt\r\n");
+    // use as_bytes to go from &str to &[u8]
+    // http://stackoverflow.com/questions/23850486/how-do-i-convert-a-string-into-a-vector-of-bytes-in-rust
+    let _ = stream.write("GET /loremipsum.txt\r\n".as_bytes());
 
     // mutable array with 3500 elements, each initialized to zero
     let mut result = [0; 3500];
@@ -15,5 +17,6 @@ fn main() {
     // mut indicates that it can be mutated
     let _ = stream.read(&mut result);
 
+    // get an "& str" from a utf8 array
     print!("{}", str::from_utf8(&result).unwrap());
 }
