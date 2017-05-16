292225d7c2018137a7acc3072fd5a0c88be0c4c6
Vikram Nathan <nathan.vikram@gmail.com>
2017-05-16 22:34:56 +0000

    Some scripts to automate the demo

---
+++ custom/demo_5tuple_record_registers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./record_register.sh 1024 regK_result_f0
+./record_register.sh 1024 regK_result_f1
+./record_register.sh 1024 regK_result_f2
+./record_register.sh 1024 regK_result_f3
+./record_register.sh 1024 regK_result_f4
+./record_register.sh 1024 regK_result_f5
+./record_register.sh 1024 regV_result_total_time
+./record_register.sh 1024 regV_result_total_size
+./record_register.sh 1024 regV_result_num_bursts
+python interpret_register_results.py > interpreted_results.txt
